8155a068e8e1b3efa94a57493894ecf8c6dcee22
eestelle <yosiky@list.ru>
2022-11-12 13:16:29 +0300

    finished cylinder

--- src/draw.c
+++ src/draw.c
@@ -178,7 +178,7 @@ void    draw_on_img(t_image *img, t_object *objects)
         {
             set_coordinates(&d, (t_vector3){ (double)(x - width_x) * 1 / WINDOW_X, (double)(width_y - y) * 1 / WINDOW_Y, 1});
             rotate(&d, 0, 0, 0);
-            color = trace_ray(get_viewer(NULL), &d, objects, 0/*RECURSIVE_DEPTH*/);
+            color = trace_ray(get_viewer(NULL), &d, objects, RECURSIVE_DEPTH);
             /* ee_mlx_pixel_put(img, width_x + x, width_y - y, color); */
             ee_mlx_pixel_put(img, x, y, color);
             ++x;
