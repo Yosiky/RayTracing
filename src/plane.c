fc241fb641bbb689cc8d9f2336426b9326a774bd
eestelle <yosiky@list.ru>
2022-11-13 18:16:44 +0300

    plane was normed

--- src/plane.c
+++ src/plane.c
@@ -1,14 +1,18 @@
 #include "plane.h"
 
-double   intersect_ray_plane(t_vector3 *restrict o, t_vector3 *restrict d,
+double	intersect_ray_plane(t_vector3 *restrict o, t_vector3 *restrict d,
 		void *data)
 {
-	t_plane   *plane = (t_plane *)data;
-	double      res;
-	double      dot = vector3_dot(&plane->normal, d);
-	t_vector3   v;
+	t_plane		*plane;
+	t_vector3	v;
+	double		res;
+	double		dot;
+	double		lenght;
+
+	plane = (t_plane *)data;
+	dot = vector3_dot(&plane->normal, d);
 	vector3_minus(&v, &plane->point, o);
-	double          lenght = vector3_dot(&plane->normal, &v);
+	lenght = vector3_dot(&plane->normal, &v);
 	res = INFINITY;
 	res = fmin(lenght / dot, res);
 	if (EPS < res)
@@ -16,10 +20,11 @@ double   intersect_ray_plane(t_vector3 *restrict o, t_vector3 *restrict d,
 	return (INFINITY);
 }
 
-void    get_normal_plane(t_vector3 *restrict res, __attribute__((__unused__))t_vector3 *restrict a,
+void	get_normal_plane(t_vector3 *restrict res, UNUSED t_vector3 *restrict a,
 		void *data)
 {
-	t_plane *plane = (t_plane *)data;
-	
+	t_plane	*plane;
+
+	plane = (t_plane *)data;
 	*res = plane->normal;
 }
